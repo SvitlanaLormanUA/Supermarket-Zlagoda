b2b9eb549dfc13639852c8b1809db26b5a9c87d5
Liza <ye.haponenko@ukma.edu.ua>
2025-03-15 15:58:41 +0200

    realisation of fake authorization

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,35 @@
-import React from 'react';
+import React , { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navigation from './components/Navigation';
 import Shop from './components/Shop';
 import Products from './components/Products';
 import Categories from './components/Categories';
 import ProductsInStore from './components/ProductsInStore';
+import Dashboard from './components/Dashboard';
+import Login from './components/Login';
 
 function App() {
+  
+  const [isLoggedIn, setIsLoggedIn] = useState(false); 
+
   return (
     <Router>
-      <div style={{ marginLeft: '270px', padding: '20px' }}>
-        <Navigation />
-        <Routes> { }
-          <Route path="/dashboard" /> { }
-          <Route path="/shop" element={<Shop />} /> { }
-          <Route path="/shop/products" element={<Products />} />
-          <Route path="/shop/categories" element={<Categories />} />
-          <Route path="/shop/products-in-store" element={<ProductsInStore />} />
-        </Routes> { }
-      </div>
+      {!isLoggedIn ? (
+        <Login onLogin={() => setIsLoggedIn(true)} />
+      ) : (
+        <>
+          <Navigation />
+          <div style={{ marginLeft: '270px', padding: '20px' }}>
+            <Routes>
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/shop" element={<Shop />} />
+              <Route path="/shop/products" element={<Products />} />
+              <Route path="/shop/categories" element={<Categories />} />
+              <Route path="/shop/products-in-store" element={<ProductsInStore />} />
+            </Routes>
+          </div>
+        </>
+      )}
     </Router>
   );
 }
