9f202592f376cac5360baf059c95f337c0d850bc
Liza <ye.haponenko@ukma.edu.ua>
2025-03-16 22:12:49 +0200

    added buttons to categories page

--- frontend/src/components/Categories.jsx
+++ frontend/src/components/Categories.jsx
@@ -4,7 +4,14 @@ import SearchAndBack from './SearchAndBack';
 function Categories() {
   return (
     <div className="categories-container">
-      <SearchAndBack/>
+      <div className="searchAndBackSection">
+        <SearchAndBack />
+      </div>
+      <div className="action-buttons">
+        <button className="action-button add-button">Add</button>
+        <button className="action-button edit-button ">Edit</button>
+        <button className="action-button delete-button ">Delete</button>
+      </div>
     </div>
   );
 }
