905ece9e8a003c2cd009602515e7a3a26e1c5d58
Liza <ye.haponenko@ukma.edu.ua>
2025-03-31 16:13:19 +0300

    added buttons to cards

--- frontend/src/components/CustomersCard.jsx
+++ frontend/src/components/CustomersCard.jsx
@@ -34,11 +34,21 @@ function CustomersCard() {
                     {customerCards.map((card) => (
                         <div key={card.card_number} className="customer-card">
                             <div className="card-content">
-                                <div className="card-header ">
-                                    <h3>{card.card_number}</h3>
-                                    <span>{card.cust_surname} {card.cust_name} {card.cust_patronymic}</span>
+                                <div className="card-header">
+                                    <div className="card-info">
+                                        <h3>{card.card_number}</h3>
+                                        <div className="card-actions">
+                                            <button className="edit-btn">
+                                                <img src="/assets/images/edit.png" alt="Edit" />
+                                            </button>
+                                            <button className="delete-btn">
+                                                <img src="/assets/images/delete.png" alt="Delete" />
+                                            </button>
+                                        </div>
+                                    </div>
+                                    <span className="customer-name">{card.cust_surname} {card.cust_name} {card.cust_patronymic}</span>
                                 </div>
-                                <div className="card-body">                        
+                                <div className="card-body">
                                     <p>{card.city}, {card.street}, {card.zip_code}</p>
                                     <p>Phone: {card.phone_number}</p>
                                     <p>Discount: {card.percent}%</p>
@@ -47,6 +57,7 @@ function CustomersCard() {
                         </div>
                     ))}
                 </div>
+
             </div>
             <style jsx>{`
                 .shop-search-bar {
