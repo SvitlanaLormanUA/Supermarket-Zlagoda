6130991fd7b27c70e301dd8e51f00d9cdb8ed3ee
Liza <ye.haponenko@ukma.edu.ua>
2025-04-09 22:33:37 +0300

    changed validation func

--- frontend/src/utils/Validation.js
+++ frontend/src/utils/Validation.js
@@ -1,4 +1,4 @@
-export const validateProductInStore = (newProduct, productsInStore) => {
+export const validateUniqueProductInStore = (newProduct, productsInStore) => {
     const existingUPC = productsInStore.some(product => product.UPC === newProduct.UPC);
     const existingProductId = productsInStore.some(product => product.id_product === newProduct.id_product);
 
@@ -10,7 +10,9 @@ export const validateProductInStore = (newProduct, productsInStore) => {
         alert("Product ID must be unique.");
         return false;
     }
+}
 
+export const validateProductInStore = (newProduct) => {
     const requiredFields = ['UPC', 'id_product', 'selling_price', 'products_number', 'promotional_product'];
     for (let field of requiredFields) {
         if (field !== 'UPC_prom' && !newProduct[field]) {
@@ -18,12 +20,10 @@ export const validateProductInStore = (newProduct, productsInStore) => {
             return false;
         }
     }
-
     if (newProduct['UPC_prom'] && (newProduct['promotional_product'] === false || newProduct['promotional_product'] === undefined)) {
         alert("If UPC_prom is filled, Promotional Product must be true.");
         return false;
     }
-
     if (newProduct['promotional_product'] === true && !newProduct['UPC_prom']) {
         alert("If Promotional Product is true, UPC_prom must be filled.");
         return false;
