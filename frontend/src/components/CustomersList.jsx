3507966865f1ae82a6177be481214c1dc1cb2991
Liza <ye.haponenko@ukma.edu.ua>
2025-04-21 15:03:10 +0300

    deleted unnecessary buttons

--- frontend/src/components/CustomersList.jsx
+++ frontend/src/components/CustomersList.jsx
@@ -9,14 +9,6 @@ const CustomersList = ({ customerCards }) => {
                         <div className="card-header">
                             <div className="card-info">
                                 <h3>{card.card_number}</h3>
-                                <div className="card-actions">
-                                    <button className="edit-btn">
-                                        <img src="/assets/images/edit.png" alt="Edit" />
-                                    </button>
-                                    <button className="delete-btn">
-                                        <img src="/assets/images/delete.png" alt="Delete" />
-                                    </button>
-                                </div>
                             </div>
                             <span className="customer-name">{card.cust_surname} {card.cust_name} {card.cust_patronymic}</span>
                         </div>
