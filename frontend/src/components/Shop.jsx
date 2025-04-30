7a838188b4dbde58bcc25fcdd4474f656d6b9f34
Liza <ye.haponenko@ukma.edu.ua>
2025-04-30 21:41:40 +0300

    deleted unnecessary import

--- frontend/src/components/Shop.jsx
+++ frontend/src/components/Shop.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import api from '../axios'; 
-import SearchAndBack from './SearchAndBack';
 
 function Shop({ userRole }) {
   const [totalPrice, setTotalPrice] = useState(null);
